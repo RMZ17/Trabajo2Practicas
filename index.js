6d538af6aee4228a0e011401e88172c08b6c3b18
RMZ17 <125528147+RMZ17@users.noreply.github.com>
2023-02-22 22:04:10 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.titulo',{
+        duration: 3000
+    });
+
+    sr.reveal('.mensual',{
+        duration: 3000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.anual',{
+        duration: 3000,
+        origin: 'left',
+        distance: '-100px'
+    });
+
+    sr.reveal('.formato',{
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
